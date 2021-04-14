7e0c1c5e78954c66e5c4c7b626c53c3ab4161edd
Kis Viktor <kisviktor85@gmail.com>
2021-04-14 09:38:54 +0200

    PHP 7.4 update
    PHP 7.4 update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.4',
+    'version' => '1.0.4.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'mrpamacs/tcpdi',
+  ),
+  'versions' => 
+  array (
+    'mrpamacs/tcpdi' => 
+    array (
+      'pretty_version' => '1.0.4',
+      'version' => '1.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'tecnickcom/tcpdf' => 
+    array (
+      'pretty_version' => '6.4.1',
+      'version' => '6.4.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5ba838befdb37ef06a16d9f716f35eb03cb1b329',
+    ),
+  ),
+);
